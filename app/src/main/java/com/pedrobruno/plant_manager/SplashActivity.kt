8032cbc659861053e8b291d7693b829b87cfc766
Pedro Bruno Fernandes <pbruno1283@gmail.com>
2022-03-10 20:43:58 -0300

    SplashScreen
    Imagem da splashScreen adicionada e background criado.

---
+++ app/src/main/java/com/pedrobruno/plant_manager/SplashActivity.kt
@@ -0,0 +1,11 @@
+package com.pedrobruno.plant_manager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
