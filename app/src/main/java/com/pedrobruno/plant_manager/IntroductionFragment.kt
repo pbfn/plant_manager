885a020eb63ad4ce8f718347d13ded33f5586794
Pedro Bruno Fernandes <pbruno1283@gmail.com>
2022-03-10 23:01:45 -0300

    TextChangeListener
    Implementado método que verifica que se o editText foi preenchido e altera o ícone e o botão caso tenha sido.

--- app/src/main/java/com/pedrobruno/plant_manager/IntroductionFragment.kt
+++ app/src/main/java/com/pedrobruno/plant_manager/IntroductionFragment.kt
@@ -1,14 +1,20 @@
 package com.pedrobruno.plant_manager
 
+import android.content.res.Resources
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.pedrobruno.plant_manager.databinding.FragmentIntroductionBinding
 
 
 class IntroductionFragment : Fragment() {
 
+    private var _binding: FragmentIntroductionBinding? = null
+    private val binding: FragmentIntroductionBinding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,8 +23,56 @@ class IntroductionFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.fragment_introduction, container, false)
+    ): View = FragmentIntroductionBinding.inflate(inflater, container, false).apply {
+        _binding = this
+    }.root
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupListenres()
+    }
+
+    private fun setupListenres() {
+        binding.editTextName.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
+
+                }
+
+                override fun onTextChanged(
+                    input: CharSequence?,
+                    start: Int,
+                    before: Int,
+                    count: Int
+                ) {
+                    if (input.isNullOrBlank() || input.isNullOrEmpty()) {
+                        binding.apply {
+                            buttonConfirm.isEnabled = false
+                            imageViewIcon.setImageDrawable(resources.getDrawable(R.drawable.ic_introduction_no_text))
+                        }
+                    } else {
+                        binding.apply {
+                            buttonConfirm.isEnabled = true
+                            imageViewIcon.setImageDrawable(resources.getDrawable(R.drawable.ic_introduction_text))
+                        }
+                    }
+                }
+
+                override fun afterTextChanged(s: Editable?) {
+
+                }
+
+            })
+        
     }
 
 }
