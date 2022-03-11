75e2865063b7d27561cf59a15b0bdea9281d6ed7
Pedro Bruno Fernandes <pbruno1283@gmail.com>
2022-03-10 21:35:32 -0300

    GoToHomePage
    Navegação da Splash para a home page adicionada

--- app/src/main/java/com/pedrobruno/plant_manager/SplashActivity.kt
+++ app/src/main/java/com/pedrobruno/plant_manager/SplashActivity.kt
@@ -1,11 +1,20 @@
 package com.pedrobruno.plant_manager
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        val splashScreenTimeOut = 2000L
+        val homeIntent = Intent(this@SplashActivity, MainActivity::class.java)
+        Handler().postDelayed({
+            startActivity(homeIntent)
+            finish()
+        }, splashScreenTimeOut)
     }
 }
