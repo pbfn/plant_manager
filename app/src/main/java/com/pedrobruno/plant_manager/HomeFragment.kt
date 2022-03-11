7e41640ea96725482c51692deeabaea159a62390
Pedro Bruno Fernandes <pbruno1283@gmail.com>
2022-03-10 22:34:12 -0300

    GoToIntroducionpage
    Navegação criada da homePage para a IntroducionPage

--- app/src/main/java/com/pedrobruno/plant_manager/HomeFragment.kt
+++ app/src/main/java/com/pedrobruno/plant_manager/HomeFragment.kt
@@ -5,9 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.pedrobruno.plant_manager.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding: FragmentHomeBinding get() = _binding!!
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,8 +22,26 @@ class HomeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View = FragmentHomeBinding.inflate(inflater, container, false).apply {
+        _binding = this
+    }.root
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupListeners()
+    }
+
+    private fun setupListeners() {
+        binding.buttonNextPage.setOnClickListener {
+            findNavController().navigate(
+                HomeFragmentDirections.actionHomeFragmentToIntroductionFragment()
+            )
+        }
     }
 
 }
--- app/src/main/java/com/pedrobruno/plant_manager/MainActivity.kt
+++ app/src/main/java/com/pedrobruno/plant_manager/MainActivity.kt
@@ -2,10 +2,20 @@ package com.pedrobruno.plant_manager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.pedrobruno.plant_manager.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding: ActivityMainBinding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
