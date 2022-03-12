b27cff6e71d8844350423d99b6ffb9962d5deff7
Pedro Bruno Fernandes <pbruno1283@gmail.com>
2022-03-12 11:40:27 -0300

    SaveUser
    Realizada a implementação para salvar o usuário digitado no fragment.

--- app/src/main/java/com/pedrobruno/plant_manager/presentation/ui/fragments/IntroductionFragment.kt
+++ app/src/main/java/com/pedrobruno/plant_manager/presentation/ui/fragments/IntroductionFragment.kt
@@ -7,14 +7,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.pedrobruno.plant_manager.R
 import com.pedrobruno.plant_manager.databinding.FragmentIntroductionBinding
+import com.pedrobruno.plant_manager.presentation.viewmodel.IntroductionViewModel
+import org.koin.androidx.viewmodel.ext.android.getViewModel
 
 
 class IntroductionFragment : Fragment() {
 
     private var _binding: FragmentIntroductionBinding? = null
     private val binding: FragmentIntroductionBinding get() = _binding!!
+    private val introductionViewModel: IntroductionViewModel by lazy {
+        getViewModel()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,45 +40,65 @@ class IntroductionFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        observeData()
         setupListenres()
     }
 
     private fun setupListenres() {
         binding.editTextName.addTextChangedListener(object : TextWatcher {
-                override fun beforeTextChanged(
-                    s: CharSequence?,
-                    start: Int,
-                    count: Int,
-                    after: Int
-                ) {
+            override fun beforeTextChanged(
+                s: CharSequence?,
+                start: Int,
+                count: Int,
+                after: Int
+            ) {
 
-                }
+            }
 
-                override fun onTextChanged(
-                    input: CharSequence?,
-                    start: Int,
-                    before: Int,
-                    count: Int
-                ) {
-                    if (input.isNullOrBlank() || input.isNullOrEmpty()) {
-                        binding.apply {
-                            buttonConfirm.isEnabled = false
-                            imageViewIcon.setImageDrawable(resources.getDrawable(R.drawable.ic_introduction_no_text))
-                        }
-                    } else {
-                        binding.apply {
-                            buttonConfirm.isEnabled = true
-                            imageViewIcon.setImageDrawable(resources.getDrawable(R.drawable.ic_introduction_text))
-                        }
+            override fun onTextChanged(
+                input: CharSequence?,
+                start: Int,
+                before: Int,
+                count: Int
+            ) {
+                if (input.isNullOrBlank() || input.isNullOrEmpty()) {
+                    binding.apply {
+                        buttonConfirm.isEnabled = false
+                        imageViewIcon.setImageDrawable(resources.getDrawable(R.drawable.ic_introduction_no_text))
+                    }
+                } else {
+                    binding.apply {
+                        buttonConfirm.isEnabled = true
+                        imageViewIcon.setImageDrawable(resources.getDrawable(R.drawable.ic_introduction_text))
                     }
                 }
+            }
 
-                override fun afterTextChanged(s: Editable?) {
+            override fun afterTextChanged(s: Editable?) {
 
-                }
+            }
 
-            })
-        
+        })
+
+        binding.buttonConfirm.setOnClickListener {
+            saveUser()
+        }
+    }
+
+    private fun saveUser() {
+        introductionViewModel.saveUser(
+            binding.editTextName.text.toString()
+        )
+    }
+
+    private fun observeData() {
+        introductionViewModel.user.observe(viewLifecycleOwner, {
+            if (it) {
+                Toast.makeText(context, "Salvo com sucesso", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(context, "Houve algum problema", Toast.LENGTH_LONG).show()
+            }
+        })
     }
 
 }
--- app/src/main/java/com/pedrobruno/plant_manager/presentation/viewmodel/IntroductionViewModel.kt
+++ app/src/main/java/com/pedrobruno/plant_manager/presentation/viewmodel/IntroductionViewModel.kt
@@ -1,10 +1,29 @@
 package com.pedrobruno.plant_manager.presentation.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.pedrobruno.plant_manager.domain.usecase.IntroductionUseCase
 
 class IntroductionViewModel(
-    private val IntroductionUseCase: IntroductionUseCase
+    private val introductionUseCase: IntroductionUseCase
 ) : ViewModel() {
+
+    private var _user = MutableLiveData<Boolean>()
+    var user: LiveData<Boolean> = _user
+
+    fun saveUser(name: String) {
+        introductionUseCase(
+            params = IntroductionUseCase.Params(
+                name = name
+            ),
+            onSuccess = {
+                _user.postValue(it)
+            },
+            onError = {
+                _user.postValue(false)
+            }
+        )
+    }
 
 }
